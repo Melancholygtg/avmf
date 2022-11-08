cc4bd474a01c7c41bf3e9f06be3393c3f5961818
Melancholygtg <114521985+Melancholygtg@users.noreply.github.com>
2022-11-08 17:44:55 -0500

    Q5 test cases

--- src/main/java/org/avmframework/examples/inputdatageneration/line/TestCaseDemoLine.java
+++ src/main/java/org/avmframework/examples/inputdatageneration/line/TestCaseDemoLine.java
@@ -7,13 +7,59 @@ import org.junit.Test;
 import static org.avmframework.examples.inputdatageneration.triangle.Triangle.TriangleType.NOT_A_TRIANGLE;
 
 public class TestCaseDemoLine extends TestCase {
-    //u2 == 0
+
+
+    //u2 != 0, 0 <= u1, u1 <= 1, 0 <= u02, u02 <= 1
     public void test1(){
         //assertTrue();
     }
 
-    //u2 != 0
+    //u2 != 0, 0 > u1, u1 <= 1, 0 <= u02, u02 <= 1
     public void test2(){
         //assertFalse();
     }
+
+    //u2 == 0, u1t == 0
+    public void test3(){
+        //assertTrue();
+    }
+
+    //u2 == 0, u2t == 0
+    public void test4(){
+        //assertTrue();
+    }
+
+    // u2 == 0, u1t != 0, u2t != 0
+    public void test5(){
+        //assertFalse();
+    }
+
+    //Statement coverage is to Select enough test cases so that every executable statement
+    //in the program is executed at least once.
+
+    //There are three branches of the intersect method are not covered by your test suite
+    //which are listed below.
+
+    //u2 != 0, 0 <= u1, u1 > 1, 0 <= u02, u02 <= 1
+    /*
+    public void test6(){
+        //assertFalse();
+    }
+    */
+
+    //u2 != 0, 0 <= u1, u1 <= 1, 0 > u02, u02 <= 1
+    /*
+    public void test7(){
+        //assertFalse();
+    }
+    */
+
+    //u2 != 0, 0 <= u1, u1 <= 1, 0 <= u02, u02 > 1
+    /*
+    public void test8(){
+        //assertFalse();
+    }
+    */
+
+
 }
