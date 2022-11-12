0997409794dcf35738c090861494a0d2895e0f31
Donglin-Wang <wdl448@yahoo.com>
2022-11-12 17:42:30 -0500

    Changed getNeighbors

--- src/main/java/org/avmframework/localsearch/HillClimbingSearch.java
+++ src/main/java/org/avmframework/localsearch/HillClimbingSearch.java
@@ -17,22 +17,22 @@ public class HillClimbingSearch extends LocalSearch {
         boolean continue_searching = true;
         int bestVal = var.getValue();
         while (continue_searching) {
-            //Construct  a Vector to store generated data from getNeighbors() function
-            Vector<Integer> neighbors = getNeighbors(var.getValue());
+            // Storing generated data from getNeighbors() function
+            ArrayList<Integer> neighbors = getNeighbors(var.getValue());
             continue_searching = false;
             currentVal = objFun.evaluate(vector);
             for (Integer neighborNumber : neighbors) {
                 var.setValue(neighborNumber);
                 neighborVal = objFun.evaluate(vector);
 
-                //Update the best solution and keep searching better solution
+                // Update the best solution and keep searching better solution
                 if (neighborVal.betterThan(currentVal)) {
                     currentVal = neighborVal;
                     continue_searching = true;
                     bestVal = neighborNumber;
                 }
             }
-            //Stop searching when the iterative finishes and return the best result
+            // Stop searching when the iterative finishes and return the best result
             if (!continue_searching) {
                 var.setValue(bestVal);
                 break;
@@ -40,19 +40,18 @@ public class HillClimbingSearch extends LocalSearch {
         }
     }
 
-    private Vector<Integer> getNeighbors(int current) {
-        Vector<Integer> neighbors = new Vector<Integer>();
-        //Enter the iterative process.
-        //The program becomes the most efficiency when the iterative times is 1000 by testing
+    private ArrayList<Integer> getNeighbors(int current) {
+        ArrayList<Integer> neighbors = new ArrayList<Integer>();
+        // A neighbor size of 1000 is the best through our preliminary testing
         for (int i = 0; i < 1000; i++) {
-            //get a new neighbor
-            int neighbor = (int) ((current) * Math.random()+(current) / 2);
-            //add the neighbor to the list if not in the list
+            // Get new neighbors
+            int neighbor = (int) Math.round((current * Math.random() + current / 2));
+            // Add the neighbor to the list if not in the list
             if (!neighbors.contains(neighbor)) {
-                neighbors.addElement(neighbor);
+                neighbors.add(neighbor);
             }
         }
-        //return all neighbors found!
+
         return neighbors;
     }
 }
